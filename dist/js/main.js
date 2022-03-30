56b4ae4ccf140960971d678dfdf00f84da422711
Vitaly Zakatov <answer-0885@mail.ru>
2022-03-30 14:43:49 -0700

    Установка Реакт

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";new class{constructor(o,t,i){this.width=o,this.height=t,this.count=i}nextSlide(){console.log("Moving forward")}prevSlide(){console.log("Moving back")}whoAmi(){console.log(this.width,this.height,this.count)}}(400,300,5).whoAmi(),console.log("button")})();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+import Slider, {
+   button
+} from './modules/script';
+
+const slider = new Slider(400, 300, 5)
+slider.whoAmi();
+console.log(button());
